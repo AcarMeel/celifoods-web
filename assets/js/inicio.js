e0030d36d36c107ba7bbd6af97a637353ed3ca0d
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2024-04-21 00:29:11 -0600

    Fix spacing icons

--- assets/js/inicio.js
+++ assets/js/inicio.js
@@ -1,17 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navbarTogglerButton = document.querySelector(".navbar-toggler");
+    const heroSocialMobile = this.documentElement.querySelector('.hero-social-media');
+    
     navbarTogglerButton.addEventListener("click", function() {
         const visualHero = document.getElementById('visualHero');
         const hasClass = visualHero.classList.contains('hide');
+        heroSocialMobile.classList.remove('show');
+        heroSocialMobile.classList.add('hide');
         if (!hasClass) {
             visualHero.classList.add('hide');
         } else {
             visualHero.classList.remove('hide');
         }
+        const timerId = setTimeout(() => {
+            heroSocialMobile.classList.remove('hide');
+            heroSocialMobile.classList.add('show');
+        }, 1500);
+        // if (timerId) {
+        //     clearTimeout(timerId);
+        // }
     });
 
     document.getElementById('btn-ordenar').addEventListener('click', function() {
         window.location.href = 'productos.html';
     }); 
+
+    
+    const timerId2 = setTimeout(() => {
+        heroSocialMobile.classList.add('show');
+    }, 1500);
+    // if (timerId2) {
+    //     clearTimeout(timerId2);
+    // }
 });
 
