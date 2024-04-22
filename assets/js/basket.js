edfac8d733740a9245df6fe089959c3d61a3e051
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2024-04-21 23:48:15 -0600

    basket

---
+++ assets/js/basket.js
@@ -0,0 +1,52 @@
+function updateBasketQuantity(quantity, nuevoProducto) {
+    let basketData = JSON.parse(localStorage.getItem("basket")) || [];
+
+    if (quantity && nuevoProducto) {
+        const existingProductIndex = basketData.findIndex(
+            (product) => product.productId === nuevoProducto.productId
+        );
+    
+        if (quantity > 0) {
+            if (existingProductIndex !== -1) {
+                basketData[existingProductIndex] = nuevoProducto;
+            } else {
+                basketData.push(nuevoProducto);
+            }
+        } else {
+            basketData = basketData.filter(
+                (product) => product.productId !== nuevoProducto.productId
+            );
+        }
+    }
+
+    const totalPrice = basketData.reduce(
+        (acc, product) => acc + product.totalProducto,
+        0
+    );
+
+    if (quantity && nuevoProducto) {
+        localStorage.setItem("basket", JSON.stringify(basketData));
+    }
+
+    const totalProducts = basketData.reduce(
+        (acc, product) => acc + product.cantidad,
+        0
+    );
+    if (window.innerWidth <= 991) {
+        const basketElement = document.getElementById(
+            "basket-menu-icon-qty-mobile"
+        );
+        basketElement.textContent = totalProducts.toString();
+    } else {
+        const basketElement = document.getElementById(
+            "basket-menu-icon-qty-desktop"
+        );
+        basketElement.textContent = totalProducts.toString();
+    }
+
+    console.log("Total price:", totalPrice);
+}
+
+window.addEventListener("load", () => {
+    updateBasketQuantity();
+  });
