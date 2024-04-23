d538b2792a4883c1730a43f38a348c82079783f1
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2024-04-22 19:00:26 -0600

    adding aos

--- assets/js/celifoods.js
+++ assets/js/celifoods.js
@@ -4,3 +4,4 @@ const jsonFilePath = "./assets/data/productos-populares.json";
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+AOS.init();
