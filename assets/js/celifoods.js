695ed4de884900bd37cdfd33fe16efa5e0baf0c7
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2024-04-23 22:22:28 -0600

    Slider

--- assets/js/celifoods.js
+++ assets/js/celifoods.js
@@ -1,7 +1,19 @@
 const jsonFilePath = "./assets/data/productos-populares.json";
 
-
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
 
 AOS.init();
+
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+  });
+});
